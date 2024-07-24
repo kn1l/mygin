bec2740f690bdaabe81b438c8b2d4f8307f93333
l1nk4i <l1nk@foxmail.com>
2024-07-24 19:33:55 +0800

    feat: use middlewares

--- mygin.go
+++ mygin.go
@@ -19,6 +19,7 @@ type Engine struct {
 // implements Handler
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	context := engine.newContext(w, req)
+	println(len(context.handlers))
 	// handlers
 	for context.index < len(context.handlers) {
 		context.Next()
@@ -44,23 +45,56 @@ func Default() *Engine {
 func (engine *Engine) addRoute(method, path string, handlers HandlerFuncChain) {
 	root := engine.trees.getMethodTree(method)
 	node := root.insert(path)
-	node.handlers = append(node.handlers, handlers...)
+	node.handlers = handlers
 }
 
 func (engine *Engine) Group(absolutePath string, handlers ...HandlerFunc) *RouterGroup {
-	return &RouterGroup{
+	if absolutePath == "/" {
+		return &engine.RouterGroup
+	}
+	group := &RouterGroup{
 		fullPath: absolutePath,
-		Handlers: handlers,
 		engine:   engine,
 	}
+	group.Handlers = append(engine.RouterGroup.Handlers, handlers...)
+	return group
+}
+
+func (engine *Engine) Use(handlers ...HandlerFunc) {
+	engine.RouterGroup.Handlers = append(engine.RouterGroup.Handlers, handlers...)
 }
 
 func (engine *Engine) Handle(method, path string, handlers ...HandlerFunc) {
-	engine.addRoute(method, path, handlers)
+	mergedHandlers := append(engine.RouterGroup.Handlers, handlers...)
+	engine.addRoute(method, path, mergedHandlers)
 }
 
 func (engine *Engine) GET(path string, handlers ...HandlerFunc) {
 	engine.Handle(http.MethodGet, path, handlers...)
+}
+
+func (engine *Engine) POST(path string, handlers ...HandlerFunc) {
+	engine.Handle(http.MethodPost, path, handlers...)
+}
+
+func (engine *Engine) PUT(path string, handlers ...HandlerFunc) {
+	engine.Handle(http.MethodPut, path, handlers...)
+}
+
+func (engine *Engine) DELETE(path string, handlers ...HandlerFunc) {
+	engine.Handle(http.MethodDelete, path, handlers...)
+}
+
+func (engine *Engine) HEAD(path string, handlers ...HandlerFunc) {
+	engine.Handle(http.MethodHead, path, handlers...)
+}
+
+func (engine *Engine) OPTIONS(path string, handlers ...HandlerFunc) {
+	engine.Handle(http.MethodOptions, path, handlers...)
+}
+
+func (engine *Engine) PATCH(path string, handlers ...HandlerFunc) {
+	engine.Handle(http.MethodPatch, path, handlers...)
 }
 
 func (engine *Engine) Run(addr ...string) error {
--- routergroup.go
+++ routergroup.go
@@ -14,20 +14,42 @@ func (group *RouterGroup) Group(relativePath string, handlers ...HandlerFunc) *R
 	return group.engine.Group(absolutePath, mergedHandlers...)
 }
 
-// func (group *RouterGroup) addRoute(method, path string, handlers HandlerFuncChain) {
-// 	root := group.engine.trees.getMethodTree(method)
-// 	node := root.insert(path)
-// 	node.handlers = handlers
-// }
+func (group *RouterGroup) Use(handlers ...HandlerFunc) {
+	group.Handlers = append(group.Handlers, handlers...)
+}
 
 func (group *RouterGroup) Handle(method, relativePath string, handlers ...HandlerFunc) {
 	absolutePath := group.calcAbsolutePath(relativePath)
-	println(absolutePath)
-	group.engine.addRoute(method, absolutePath, handlers)
+	mergedHandlers := append(group.Handlers, handlers...)
+	group.engine.addRoute(method, absolutePath, mergedHandlers)
 }
 
 func (group *RouterGroup) GET(relativePath string, handlers ...HandlerFunc) {
 	group.Handle(http.MethodGet, relativePath, handlers...)
+}
+
+func (group *RouterGroup) POST(path string, handlers ...HandlerFunc) {
+	group.Handle(http.MethodPost, path, handlers...)
+}
+
+func (group *RouterGroup) PUT(path string, handlers ...HandlerFunc) {
+	group.Handle(http.MethodPut, path, handlers...)
+}
+
+func (group *RouterGroup) DELETE(path string, handlers ...HandlerFunc) {
+	group.Handle(http.MethodDelete, path, handlers...)
+}
+
+func (group *RouterGroup) HEAD(path string, handlers ...HandlerFunc) {
+	group.Handle(http.MethodHead, path, handlers...)
+}
+
+func (group *RouterGroup) OPTIONS(path string, handlers ...HandlerFunc) {
+	group.Handle(http.MethodOptions, path, handlers...)
+}
+
+func (group *RouterGroup) PATCH(path string, handlers ...HandlerFunc) {
+	group.Handle(http.MethodPatch, path, handlers...)
 }
 
 func (group *RouterGroup) calcAbsolutePath(relativePath string) string {
